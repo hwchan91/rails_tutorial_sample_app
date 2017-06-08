7a94e1f941a8a64838cbcdc3aa516b206c2950d5
hwchan91 <hwchan91@gmail.com>
2017-06-08 16:41:45 +0800

    Finish user edit, update, index and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,38 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  test "login page when not logged-in" do
+    get login_path
+    assert_template 'sessions/new'
+    assert_select "a[href=?]", root_path,        count: 2
+    assert_select "a[href=?]", help_path
+      assert_select "a[href=?]", users_path,     count: 0
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://news.railstutorial.org/"
+  end
+
+  test "login page when logged-in" do
+    log_in_as(@user)
+    get login_path
+    assert_template 'sessions/new'
+    assert_select "a[href=?]", root_path,        count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", login_path,       count:0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://news.railstutorial.org/"
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
