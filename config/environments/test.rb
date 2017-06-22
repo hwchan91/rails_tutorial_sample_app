be5639b3d08896ccc1dcb7c4db1247bc8b8ab9e7
hwchan91 <hwchan91@gmail.com>
2017-06-22 13:41:19 +0800

    Add user microposts

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  #require 'carrierwave/orm/activerecord'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAIEI3JW7PL5DTZ74Q'],
+      :aws_secret_access_key => ENV['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'],
+      :region                => ENV['us-east-2']
+      }
+    config.fog_directory     =  ENV['railstutorial.hwchan91']
+  end
+end
